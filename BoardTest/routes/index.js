2f0fb11e7c604efc178c138a49aa27a1fc42704a
sim <sim@192.168.25.22>
2017-01-01 17:25:00 +0900

    modify

--- BoardTest/routes/index.js
+++ BoardTest/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('*', function(req, res) {
 	console.log("index.page 호출");
 	res.sendFile("/views/index.html");
 });
