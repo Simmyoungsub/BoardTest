c07c85f524dc24ee4fb63c2463a61871261b3345
sim <sim@192.168.25.22>
2017-01-01 23:20:46 +0900

    수정

--- BoardTest/routes/boardSchema.js
+++ BoardTest/routes/boardSchema.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var boardSchema = new Schema({
-	"username" : String
+	username : String
 });
 
 module.exports = mongoose.model('user', boardSchema);
