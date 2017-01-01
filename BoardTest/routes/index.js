37631a104f3597736731ddc24cc606f029fee3cf
sim <sim@192.168.25.22>
2017-01-01 18:06:41 +0900

    index.html 호출

--- BoardTest/routes/index.js
+++ BoardTest/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res) {
 	console.log("index.page 호출");
 	console.log(__dirname);
 	console.log(path);
-	res.sendFile(path.join("../views/index.html"));
+	res.render("index.html")
 });
 
 console.log("call");
