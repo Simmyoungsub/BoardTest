dfca9975ff155e058737af8089c958dfbfcac798
sim <sim@192.168.25.22>
2017-01-01 22:09:33 +0900

    list 수정

--- BoardTest/routes/list.js
+++ BoardTest/routes/list.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Board = require('./boardSchema');
 
-router.get('/', function(req, res, next) {
-	 Board.find().exec(function(err,res){
-		console.log(res); 
+router.get('/', function(req, res) {
+	 Board.find({}).exec(function(err,rows){
+		console.log(rows); 
 	 });
 });
 
