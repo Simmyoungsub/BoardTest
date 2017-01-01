49b6c3f9241e9fa0cc0ab7d06b28ad02945d5f6c
sim <sim@192.168.25.22>
2017-01-01 17:39:21 +0900

    index.html 호출

--- BoardTest/routes/index.js
+++ BoardTest/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
 	console.log("index.page 호출");
-	res.sendFile("index.html");
+	console.log(__dirname);
+	res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 console.log("call");
